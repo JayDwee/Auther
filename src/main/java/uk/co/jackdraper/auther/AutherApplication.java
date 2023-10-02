c9112abe7b4927b3745c7ceabab36a281229b606
Jack Draper <drapj002@gmail.com>
2023-10-02 20:02:20 +0100

    chore: initial commit

---
+++ src/main/java/uk/co/jackdraper/auther/AutherApplication.java
@@ -0,0 +1,13 @@
+package uk.co.jackdraper.auther;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/jackdraper/auther/AutherApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.jackdraper.auther;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
